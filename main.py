93cb3e514b8dc9fd37c1b95960a0cc52bcab731f
Nikolay Fedorov <nianfedorov@edu.hse.ru>
2024-01-16 17:23:03 +0300

    Fix abbreviations of one word strings

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ CLASS_DESC_FIELDS = [
 
 
 def abbreviate(string: str):
-    if len(string.split(' ')) > 4: return string
+    if len(string.split(' ')) > 4 or len(string.split(' ')) == 1: return string
     result_parts = ['']
     for part in string.split(' '):
         if len(part) == 0:
